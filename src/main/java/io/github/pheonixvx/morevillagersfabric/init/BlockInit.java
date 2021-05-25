d0cf338dbac4bb0fbdec98c00f3f6cf457772428
zer0 <thegrimreaper.vx@gmail.com>
2021-05-25 11:31:11 -0400

    Add purpur altar and change enderian's POI

--- src/main/java/io/github/pheonixvx/morevillagersfabric/init/BlockInit.java
+++ src/main/java/io/github/pheonixvx/morevillagersfabric/init/BlockInit.java
@@ -12,10 +12,12 @@ public class BlockInit {
     public static final Block OCEANOGRAPHY_TABLE = new Block(AbstractBlock.Settings.copy(Blocks.CARTOGRAPHY_TABLE));
     public static final Block WOODWORKING_TABLE = new Block(AbstractBlock.Settings.copy(Blocks.CARTOGRAPHY_TABLE));
     public static final Block DECAYED_WORKBRENCH = new Block(AbstractBlock.Settings.copy(Blocks.CARTOGRAPHY_TABLE));
+    public static final Block PURPUR_ALTAR = new Block(AbstractBlock.Settings.copy(Blocks.END_STONE));
 
     public static void initBlocks() {
         Registry.register(Registry.BLOCK, new Identifier(MoreVillagersFabric.MOD_ID, "oceanography_table"), OCEANOGRAPHY_TABLE);
         Registry.register(Registry.BLOCK, new Identifier(MoreVillagersFabric.MOD_ID, "woodworking_table"), WOODWORKING_TABLE);
         Registry.register(Registry.BLOCK, new Identifier(MoreVillagersFabric.MOD_ID, "decayed_workbench"), DECAYED_WORKBRENCH);
+        Registry.register(Registry.BLOCK, new Identifier(MoreVillagersFabric.MOD_ID, "purpur_altar"), PURPUR_ALTAR);
     }
 }
