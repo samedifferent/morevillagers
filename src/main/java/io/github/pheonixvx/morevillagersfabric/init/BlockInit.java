2efa461750a905e919fbf404f540942edc17833a
zer0 <thegrimreaper.vx@gmail.com>
2021-05-25 11:10:52 -0400

    Change netherian POI

--- src/main/java/io/github/pheonixvx/morevillagersfabric/init/BlockInit.java
+++ src/main/java/io/github/pheonixvx/morevillagersfabric/init/BlockInit.java
@@ -11,9 +11,11 @@ public class BlockInit {
 
     public static final Block OCEANOGRAPHY_TABLE = new Block(AbstractBlock.Settings.copy(Blocks.CARTOGRAPHY_TABLE));
     public static final Block WOODWORKING_TABLE = new Block(AbstractBlock.Settings.copy(Blocks.CARTOGRAPHY_TABLE));
+    public static final Block DECAYED_WORKBRENCH = new Block(AbstractBlock.Settings.copy(Blocks.CARTOGRAPHY_TABLE));
 
     public static void initBlocks() {
         Registry.register(Registry.BLOCK, new Identifier(MoreVillagersFabric.MOD_ID, "oceanography_table"), OCEANOGRAPHY_TABLE);
         Registry.register(Registry.BLOCK, new Identifier(MoreVillagersFabric.MOD_ID, "woodworking_table"), WOODWORKING_TABLE);
+        Registry.register(Registry.BLOCK, new Identifier(MoreVillagersFabric.MOD_ID, "decayed_workbench"), DECAYED_WORKBRENCH);
     }
 }
--- src/main/java/io/github/pheonixvx/morevillagersfabric/init/VillagerInit.java
+++ src/main/java/io/github/pheonixvx/morevillagersfabric/init/VillagerInit.java
@@ -15,7 +15,7 @@ import net.minecraft.world.poi.PointOfInterestType;
 public class VillagerInit {
     // Points of Interest
     public static final PointOfInterestType OCEANOGRAPHER_POI = PointOfInterestType.register("oceanographer", PointOfInterestType.getAllStatesOf(BlockInit.OCEANOGRAPHY_TABLE), 1, 1);
-    public static final PointOfInterestType NETHERIAN_POI = PointOfInterestType.register("netherian", PointOfInterestType.getAllStatesOf(Blocks.CHISELED_POLISHED_BLACKSTONE), 1, 1);
+    public static final PointOfInterestType NETHERIAN_POI = PointOfInterestType.register("netherian", PointOfInterestType.getAllStatesOf(BlockInit.DECAYED_WORKBRENCH), 1, 1);
     public static final PointOfInterestType WOODWORKER_POI = PointOfInterestType.register("woodworker", PointOfInterestType.getAllStatesOf(BlockInit.WOODWORKING_TABLE), 1, 1);
     public static final PointOfInterestType ENDERIAN_POI = PointOfInterestType.register("enderian", PointOfInterestType.getAllStatesOf(Blocks.PURPUR_PILLAR), 1, 1);
     public static final PointOfInterestType ENGINEER_POI = PointOfInterestType.register("engineer", PointOfInterestType.getAllStatesOf(Blocks.REDSTONE_BLOCK), 1, 1);
