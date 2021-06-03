e1dcf2b83ff2ee98eaf3b057e5cdf4e8a0fb5e3d
zer0 <thegrimreaper.vx@gmail.com>
2021-06-03 18:42:36 -0400

    Add advancements and refactor structure weights for the engineer.

--- src/main/java/io/github/pheonixvx/morevillagersfabric/MoreVillagersFabric.java
+++ src/main/java/io/github/pheonixvx/morevillagersfabric/MoreVillagersFabric.java
@@ -72,34 +72,34 @@ public class MoreVillagersFabric implements ModInitializer {
             registerJigsaw(server, plainsPoolLocation, new Identifier("morevillagers:village/plains/plains_oceanographer"), 12);
             registerJigsaw(server, plainsPoolLocation, new Identifier("morevillagers:village/plains/plains_florist"), 13);
             registerJigsaw(server, plainsPoolLocation, new Identifier("morevillagers:village/plains/plains_hunter"), 15);
-            registerJigsaw(server, plainsPoolLocation, new Identifier("morevillagers:village/plains/plains_engineer"), 15);
+            registerJigsaw(server, plainsPoolLocation, new Identifier("morevillagers:village/plains/plains_engineer"), 25);
 
             // TAIGA VILLAGE HOUSES
             registerJigsaw(server, taigaPoolLocation, new Identifier("morevillagers:village/taiga/taiga_woodworker"), 12);
             registerJigsaw(server, taigaPoolLocation, new Identifier("morevillagers:village/taiga/taiga_oceanographer"), 12);
             registerJigsaw(server, taigaPoolLocation, new Identifier("morevillagers:village/taiga/taiga_florist"), 13);
             registerJigsaw(server, taigaPoolLocation, new Identifier("morevillagers:village/taiga/taiga_hunter"), 15);
-            registerJigsaw(server, taigaPoolLocation, new Identifier("morevillagers:village/taiga/taiga_engineer"), 15);
+            registerJigsaw(server, taigaPoolLocation, new Identifier("morevillagers:village/taiga/taiga_engineer"), 25);
 
             // SAVANNA VILLAGE HOUSES
             registerJigsaw(server, savannaPoolLocation, new Identifier("morevillagers:village/savanna/savanna_woodworker"), 12);
             registerJigsaw(server, savannaPoolLocation, new Identifier("morevillagers:village/savanna/savanna_oceanographer"), 12);
             registerJigsaw(server, savannaPoolLocation, new Identifier("morevillagers:village/savanna/savanna_florist"), 13);
             registerJigsaw(server, savannaPoolLocation, new Identifier("morevillagers:village/savanna/savanna_hunter"), 15);
-            registerJigsaw(server, savannaPoolLocation, new Identifier("morevillagers:village/savanna/savanna_engineer"), 15);
+            registerJigsaw(server, savannaPoolLocation, new Identifier("morevillagers:village/savanna/savanna_engineer"), 25);
 
             // SNOWY VILLAGE HOUSES
             registerJigsaw(server, snowyPoolLocation, new Identifier("morevillagers:village/snowy/snowy_woodworker"), 12);
             registerJigsaw(server, snowyPoolLocation, new Identifier("morevillagers:village/snowy/snowy_oceanographer"), 12);
             registerJigsaw(server, snowyPoolLocation, new Identifier("morevillagers:village/snowy/snowy_florist"), 13);
             registerJigsaw(server, snowyPoolLocation, new Identifier("morevillagers:village/snowy/snowy_hunter"), 15);
-            registerJigsaw(server, snowyPoolLocation, new Identifier("morevillagers:village/snowy/snowy_engineer"), 15);
+            registerJigsaw(server, snowyPoolLocation, new Identifier("morevillagers:village/snowy/snowy_engineer"), 25);
 
             // DESERT VILLAGE HOUSES
             registerJigsaw(server, desertPoolLocation, new Identifier("morevillagers:village/desert/desert_woodworker"), 12);
             registerJigsaw(server, desertPoolLocation, new Identifier("morevillagers:village/desert/desert_oceanographer"), 12);
             registerJigsaw(server, desertPoolLocation, new Identifier("morevillagers:village/desert/desert_florist"), 13);
-            registerJigsaw(server, desertPoolLocation, new Identifier("morevillagers:village/desert/desert_hunter_hunter_engineer"), 15);
+            registerJigsaw(server, desertPoolLocation, new Identifier("morevillagers:village/desert/desert_hunter_hunter_engineer"), 25);
         });
     }
 }
