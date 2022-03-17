6608d51515b5747e2ad57c3bdc4114f738608bf7
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-03-17 00:36:44 -0400

    Port to 1.18.2

---
+++ src/main/java/com/samebutdifferent/morevillagers/init/ModTags.java
@@ -0,0 +1,22 @@
+package com.samebutdifferent.morevillagers.init;
+
+import com.samebutdifferent.morevillagers.MoreVillagers;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
+
+public class ModTags {
+    public static final TagKey<ConfiguredStructureFeature<?, ?>> ON_BURIED_TREASURE_EXPLORER_MAPS = createConfiguredStructureFeatureTag("on_buried_treasure_explorer_maps");
+    public static final TagKey<ConfiguredStructureFeature<?, ?>> ON_FORTRESS_EXPLORER_MAPS = createConfiguredStructureFeatureTag("on_fortress_explorer_maps");
+    public static final TagKey<ConfiguredStructureFeature<?, ?>> ON_BASTION_REMNANT_EXPLORER_MAPS = createConfiguredStructureFeatureTag("on_bastion_remnant_explorer_maps");
+    public static final TagKey<ConfiguredStructureFeature<?, ?>> ON_END_CITY_EXPLORER_MAPS = createConfiguredStructureFeatureTag("on_end_city_explorer_maps");
+    public static final TagKey<ConfiguredStructureFeature<?, ?>> ON_SWAMP_HUT_EXPLORER_MAPS = createConfiguredStructureFeatureTag("on_swamp_hut_explorer_maps");
+    public static final TagKey<ConfiguredStructureFeature<?, ?>> ON_JUNGLE_TEMPLE_EXPLORER_MAPS = createConfiguredStructureFeatureTag("on_jungle_temple_explorer_maps");
+    public static final TagKey<ConfiguredStructureFeature<?, ?>> ON_PILLAGER_OUTPOST_EXPLORER_MAPS = createConfiguredStructureFeatureTag("on_pillager_outpost_explorer_maps");
+    public static final TagKey<ConfiguredStructureFeature<?, ?>> ON_MINESHAFT_EXPLORER_MAPS = createConfiguredStructureFeatureTag("on_mineshaft_explorer_maps");
+
+    private static TagKey<ConfiguredStructureFeature<?, ?>> createConfiguredStructureFeatureTag(String name) {
+        return TagKey.create(Registry.CONFIGURED_STRUCTURE_FEATURE_REGISTRY, new ResourceLocation(MoreVillagers.MOD_ID, name));
+    }
+}
--- src/main/java/com/samebutdifferent/morevillagers/util/JigsawHelper.java
+++ src/main/java/com/samebutdifferent/morevillagers/util/JigsawHelper.java
@@ -1,13 +1,14 @@
 package com.samebutdifferent.morevillagers.util;
 
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.data.worldgen.ProcessorLists;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.level.levelgen.feature.structures.StructurePoolElement;
-import net.minecraft.world.level.levelgen.feature.structures.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorList;
 
 import java.util.ArrayList;
@@ -19,10 +20,10 @@ public class JigsawHelper {
         Registry<StructureTemplatePool> pools = manager.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY);
         StructureTemplatePool pool = pools.get(poolLocation);
 
-        StructureProcessorList processorList = manager.registryOrThrow(Registry.PROCESSOR_LIST_REGISTRY).getOptional(poolLocation).orElse(ProcessorLists.EMPTY);
+        StructureProcessorList processorList = manager.registryOrThrow(Registry.PROCESSOR_LIST_REGISTRY).getOptional(poolLocation).orElse(ProcessorLists.EMPTY.value());
         List<StructurePoolElement> elements = pool.templates;
 
-        StructurePoolElement element = StructurePoolElement.legacy(nbtLocation.toString(), processorList).apply(StructureTemplatePool.Projection.RIGID);
+        StructurePoolElement element = StructurePoolElement.legacy(nbtLocation.toString(), Holder.direct(processorList)).apply(StructureTemplatePool.Projection.RIGID);
         for (int i = 0; i < weight; i++) {
             elements.add(element);
         }
